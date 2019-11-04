228c95896b6b8ebe81b7efdfc7fde79114c5aceb
BalicaAdrian <adriancbalica@gmail.com>
2019-11-04 20:33:55 +0200

    Homework second lab

---
+++ Homeworks/PotentiometerRGB/RGB_LED_Controlled_By_Potentiometer.ino
@@ -0,0 +1,30 @@
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
+
+const int potRedPin = A0;
+const int potGreenPin = A1;
+const int potBluePin = A2;
+
+void setup() {
+    pinMode(redPin,OUTPUT);
+    pinMode(greenPin,OUTPUT);
+    pinMode(bluePin,OUTPUT);
+
+}
+
+void loop() {
+
+  int analogValueRed = analogRead(potRedPin);
+  int analogValueGreen = analogRead(potGreenPin);
+  int analogValueBlue = analogRead( potBluePin);
+
+   int valPotRed = map(analogValueRed, 0, 1023, 0, 255);
+   int valPotGreen = map(analogValueGreen, 0, 1023, 0, 255);
+   int valPotBlue = map(analogValueBlue, 0, 1023, 0, 255);
+
+   analogWrite(redPin,valPotRed); 
+   analogWrite(greenPin,valPotGreen);
+   analogWrite(bluePin,valPotBlue);
+    
+}
